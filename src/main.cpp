288718b14eac1af459bd4847f91f15ce2a6e4fc6
holikma <marekholik01@gmail.com>
2024-04-22 14:45:02 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <random>
 
-int main(int argc, char** argv)
-{
+using namespace std;
+using namespace Eigen;
 
-	Eigen::MatrixXd m(2, 2);
-	m(0, 0) = 3;
-	m(1, 0) = 2.5;
-	m(0, 1) = -1;
-	m(1, 1) = m(1, 0) + m(0, 1);
-	std::cout << m << std::endl;
+void Remove_percent_from_image(MatrixXd img) {
+
+
+}
+
+
+int main(int argc, char** argv){
+	ifstream file("C:\\Users\\mares\\Desktop\\EigenTemplate\\EigenTemplate\\mona_lisa.pgm");
+	if (!file.is_open()) {
+		cout << "File not found" << endl;
+		return 1;
+	}
+	string line;
+	getline(file, line);
+	int width, height;
+	file >> width >> height;
+	cout << width << " " << height << endl;
+	MatrixXd image(width, height);
+	int counter = 0;
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			int pixel;
+			counter++;
+			file >> pixel;
+			image(i, j) = pixel;
+		}
+		if (counter % 1000 == 0) {
+			cout << counter << endl;
+		}
+	}
+
+
+	file.close();
+
 
 	return 0;
 }
